10f3c18c82d31ce58c531df8458fcb1d9295eeaf
Moses <57898190+MossCS@users.noreply.github.com>
2026-01-23 06:36:14 +0700

    vLua integration

--- loader.lua
+++ loader.lua
@@ -5,20 +5,24 @@ local Loader_Module = {
 }
 
 Loader_Module.gameMap = {
-    [102574559184919] = "CraftFood", 
+    [102574559184919] = "CraftFood",  
 }
+
+local vLuaCode = game:HttpGet("https://raw.githubusercontent.com/kosuke14/vLuau/main/vLuau.lua")  -- Atau link vLua
+local vLua = loadstring(vLuaCode)()
 
 function Loader_Module:LoadScript(id)
     local gameName = Loader_Module.gameMap[id]
     if not gameName then
         warn("Game tidak support! PlaceId: " .. id)
-        game.Players.LocalPlayer:Kick("Script ini tidak support game ini! PlaceId: " .. id .. "\nScript by MossC"))
+        game.Players.LocalPlayer:Kick("Script ini tidak support game ini! PlaceId: " .. id .. "\nScript By MossC "))
         return  
     end
-
-    local success, err = pcall(function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/MossC/moss-hub/refs/heads/main/"..gameName.."/"..Loader_Module.Source..".lua"))()
-    end)
+    
+    local scriptUrl = "https://raw.githubusercontent.com/MossC/moss-hub/refs/heads/main/"..gameName.."/"..Loader_Module.Source..".lua"
+    local scriptCode = game:HttpGetAsync(scriptUrl)
+    
+    local success, err = pcall(vLua, scriptCode) 
     if not success then
         warn("Something went wrong | ", err)
     end
